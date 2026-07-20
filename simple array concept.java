2942504965e1704bcb73ae1ba21f10164fd8140f
mrdhaya143-hub <mrdhaya143@gmail.com>
2026-07-20 15:19:50 +0530

    Create simple array concept.java

---
+++ simple array concept.java
@@ -0,0 +1,9 @@
+public class hello{
+    public static void main(String[] args){
+        int[] marks = {370, 480, 432, 333};
+        String[] name = {"dhaya", "dharun", "gowtham", "anbu"};
+        for (int i = 0; i < marks.length; i++){
+            System.out.println(name[i] + "=" + marks[i]);
+        }
+    }
+}
