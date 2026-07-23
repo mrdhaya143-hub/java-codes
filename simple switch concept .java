bc60dd13513674b471631476a6cb6bc50ce285c4
mrdhaya143-hub <mrdhaya143@gmail.com>
2026-07-23 16:42:05 +0530

    Add simple switch case example for days of the week

---
+++ simple switch concept .java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Main{
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number code:");
+        int day=sc.nextInt();
+        switch(day){
+            case 1:
+                System.out.println("monday");
+                break;
+                case 2:
+                    System.out.println("tuesday");
+                    break;
+                    case 3:
+                        System.out.println("wednesday");
+                        break;
+                        case 4:
+                            System.out.println("Thursday");
+                            break;
+                            case 5:
+                                System.out.println("Friday");
+                                break;
+                                default:
+                                System.out.println("Invalid day");
+        }
+    }
+}
