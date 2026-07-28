1242ad836c25a91ad796e93c2e982737f7031d1a
mrdhaya143-hub <mrdhaya143@gmail.com>
2026-07-28 14:40:14 +0530

    Create Simpleprogramforthelockscreen.java

---
+++ Simpleprogramforthelockscreen.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main{
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the Number:");
+        int a=sc.nextInt();
+        if(a==5102005)
+        {
+            System.out.println("The system unlocked");
+        }
+        else
+            System.out.println("Please enter the correct password");
+        }
+        }
+    
