3657284f1bdd18d1779677cfc0db6d97e51c324b
mrdhaya143-hub <mrdhaya143@gmail.com>
2026-07-30 16:21:55 +0530

    Add program to calculate area of a circle

---
+++ programfortheradiusofthecircle.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Radius of the circle:");
+        long A=sc.nextInt();
+        long D=A*A;
+      Double B=3.14;
+        double E=D*B;
+        System.out.println("The area of the circle is ="+E);
+    }
+}
